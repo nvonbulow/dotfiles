7e2f874057411a56b5a6659e8ee4d9f58e4791e4
Nick von Bulow <nick@nvonbulow.com>
2019-10-03 16:33:18 -0400

    Add thefuck to zsh config

--- shells/zsh/configs/plugins.zsh
+++ shells/zsh/configs/plugins.zsh
@@ -5,4 +5,5 @@ zplug "zsh-users/zsh-history-substring-search", defer:2
 # Remember to install `pkgfile` if on Arch Linux
 zplug "plugins/command-not-found", from:oh-my-zsh
 zplug "plugins/gpg-agent", from:oh-my-zsh
+zplug "plugins/thefuck", from:oh-my-zsh
 
