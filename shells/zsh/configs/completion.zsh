494605ca5ba79c9657e414450e73054f468224f6
Nick von Bulow <nick@nvonbulow.com>
2021-06-01 11:30:16 -0400

    Enhancd (disabled)

--- shells/zsh/configs/completion.zsh
+++ shells/zsh/configs/completion.zsh
@@ -4,4 +4,6 @@ HYPHEN_INSENSITIVE=true
 
 # oh-my-zsh style completion (case-insensitive, etc.)
 zplug 'lib/completion', from:oh-my-zsh
+# Enhanced version of the cd command with fuzzy search
+# zplug 'b4b4r07/enhancd', use:init.sh
 
