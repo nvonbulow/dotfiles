d7af545abfdc827fadb92e4c2924dfe4df3b3bca
Nick von Bulow <nick@nvonbulow.com>
2021-06-09 23:43:33 -0400

    Add WSL alias for the Windows command line

--- shells/zsh/configs/aliases.zsh
+++ shells/zsh/configs/aliases.zsh
@@ -1,6 +1,9 @@
 alias zshconfig="edit ~/.zshrc"
 alias zshsource="source ~/.zshrc"
 alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done'
+case $(uname -a) in
+    *microsoft*) alias cmd="/mnt/c/Windows/System32/cmd.exe" ;;
+esac
 
 # oh-my-zsh directory aliases
 zplug "lib/directories", from:oh-my-zsh
