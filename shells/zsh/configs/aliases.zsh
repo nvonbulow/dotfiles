7b777147f22e8972d5e994037ebe40fdedbc4f13
Nick von Bulow <nick@nvonbulow.com>
2021-06-15 11:12:04 -0400

    Fix autocompletion for git on WSL

--- shells/zsh/configs/aliases.zsh
+++ shells/zsh/configs/aliases.zsh
@@ -1,3 +1,5 @@
+setopt completealiases
+
 alias zshconfig="edit ~/.zshrc"
 alias zshsource="source ~/.zshrc"
 alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done'
