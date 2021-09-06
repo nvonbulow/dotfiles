9a1c595eec3eeb026aefaef1c996e827bcb3e154
Nick von Bulow <nick@nvonbulow.com>
2021-09-06 00:08:05 -0400

    Remove rarely used plugin

--- shells/zsh/configs/plugins.zsh
+++ shells/zsh/configs/plugins.zsh
@@ -5,7 +5,6 @@ zplug "zsh-users/zsh-history-substring-search", defer:2
 # Remember to install `pkgfile` if on Arch Linux
 zplug "plugins/command-not-found", from:oh-my-zsh
 zplug "plugins/gpg-agent", from:oh-my-zsh
-zplug "plugins/thefuck", from:oh-my-zsh
 
 # Interactive git command (igit)
 zplug "ytakahashi/igit"
