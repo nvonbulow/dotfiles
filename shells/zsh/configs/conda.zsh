879feea3ad2e105406c6e294927042673de7a380
Nick von Bulow <nick@nvonbulow.com>
2021-11-28 19:37:42 -0500

    Add support for locally installed anaconda

---
+++ shells/zsh/configs/conda.zsh
@@ -0,0 +1,17 @@
+__conda_path=$HOME/anaconda3
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$__conda_path/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$__conda_path/etc/profile.d/conda.sh" ]; then
+        . "$__conda_path/etc/profile.d/conda.sh"
+    else
+        export PATH="$__conda_path/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+unset __conda_path
+# <<< conda initialize <<<
