a292e825fe42786fe660af5d744a816be3498c16
Nick von Bulow <nick@nvonbulow.com>
2022-06-21 11:49:11 -0400

    Add iTerm2 integration

--- home/dot_zsh/exact_configs/shell.zsh
+++ home/dot_zsh/exact_configs/shell.zsh
@@ -3,3 +3,8 @@
 case $(uname -a) in
     *Microsoft*) unsetopt BG_NICE ;;
 esac
+
+# iTerm2 shell integration on MacOS
+if [[ -f $HOME/.zsh/iterm2_integration.zsh ]]; then
+    source $HOME/.zsh/iterm2_integration.zsh
+fi
