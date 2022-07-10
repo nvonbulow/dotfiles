4f9ee74b47db8feafc8b7d60cb9726298e9c185d
nvonbulow <nick@nvonbulow.com>
2022-07-10 01:46:38 -0400

    Easier support for goenv

--- home/dot_zsh/exact_configs/env.zsh
+++ home/dot_zsh/exact_configs/env.zsh
@@ -21,13 +21,8 @@ elif [[ -e /usr/share/nvm/init-nvm.sh ]]; then
   source /usr/share/nvm/init-nvm.sh
 fi
 
-export GOENV_ROOT=$HOME/.goenv
+# Automatic goenv download and installation
+zplug "RiverGlide/zsh-goenv", from:gitlab
 
-if [[ -d $GOENV_ROOT ]]; then
-  _add_to_path $GOENV_ROOT/bin
-  eval "$(goenv init -)"
-else
-  unset GOENV_ROOT
-fi
 # Android SDK path variable
 export ANDROID_HOME=/opt/android-sdk
