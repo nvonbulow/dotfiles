aa2409f333f59e28b8e36c17d0e00cb6e92a7659
nvonbulow <nick@nvonbulow.com>
2022-11-12 15:31:15 -0500

    WIP starship

--- home/dot_zsh/exact_configs/theme.zsh
+++ home/dot_zsh/exact_configs/theme.zsh
@@ -1,27 +1,5 @@
 # Powerlevel10k Theme - This is basically a faster version of powerlevel9k
 # zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
-zplug romkatv/powerlevel10k, as:theme, depth:1
+# zplug romkatv/powerlevel10k, as:theme, depth:1
 
-# Old configuration; see new p10k.zsh
-
-# POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(host root_indicator dir_writable dir vcs)
-# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time background_jobs status time ssh)
-# POWERLEVEL9K_MODE="nerdfont-complete"
-# 
-# DEFAULT_FOREGROUND=006 DEFAULT_BACKGROUND=255
-# DEFAULT_COLOR=$DEFAULT_FOREGROUND
-# 
-# POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR="\uE0B4"
-# POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR="%F{$(( $DEFAULT_BACKGROUND - 2 ))}|%f"
-# POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR="\uE0B6"
-# POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR="%F{$(( $DEFAULT_BACKGROUND - 2 ))}|%f"
-# 
-# POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="\u256D"
-# # POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX="\u2771"
-# POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="\u2570 \uF460\uF460\uF460 "
-# 
-# POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-# POWERLEVEL9K_SHORTEN_DELIMITER=""
-# POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
-
+eval $(starship init zsh)
