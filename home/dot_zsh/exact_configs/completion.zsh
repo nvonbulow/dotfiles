8b0a5f0c45078d974e74135ab6800ba496326e72
nvonbulow <nick@nvonbulow.com>
2023-09-06 22:21:19 -0400

    Add brew zsh completions

--- home/dot_zsh/exact_configs/completion.zsh
+++ home/dot_zsh/exact_configs/completion.zsh
@@ -2,8 +2,12 @@
 CASE_SENSITIVE=false
 HYPHEN_INSENSITIVE=true
 
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
+
 # oh-my-zsh style completion (case-insensitive, etc.)
 zplug 'lib/completion', from:oh-my-zsh
 # Enhanced version of the cd command with fuzzy search
 # zplug 'b4b4r07/enhancd', use:init.sh
-
