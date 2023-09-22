06a37faa81ed108cf5bdfcff26079b3598a7b776
nvonbulow <nick@nvonbulow.com>
2023-09-22 14:07:19 -0400

    Update exa to eza - exa is deprecated

--- home/dot_zsh/exact_configs/aliases.zsh
+++ home/dot_zsh/exact_configs/aliases.zsh
@@ -22,8 +22,8 @@ if whence -w git | grep alias > /dev/null; then
 fi
 
 # If exa is installed, alias ls to it
-if [ "$(command -v exa)" ]; then
-    alias ls="exa --icons"
+if [ "$(command -v eza)" ]; then
+    alias ls="eza --icons"
 fi
 
 # Are we running on WSL?
@@ -35,7 +35,6 @@ if [[ -d "/mnt/c/Windows" ]]; then
         alias git="_wsl_autogit"
     }
 fi
-
 
 function git-cleanup {
     git fetch --prune && git branch -vv | grep ': gone]' | grep -v "\*" | awk '{ print $1 }' | xargs git branch -D
