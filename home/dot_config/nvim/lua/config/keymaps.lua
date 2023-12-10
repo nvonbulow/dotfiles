d54b7186f1b551997f40a288effd7147f1832f46
nvonbulow <nick@nvonbulow.com>
2023-12-10 14:12:06 -0500

    Add WinShift Plugin for moving windows around

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "tt", "<cmd>terminal<cr>i", { desc = "Open terminal in current window" })
+vim.keymap.set("n", "<leader>bn", "<cmd>enew<cr>", { desc = "New empty buffer" })
--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -11,6 +11,65 @@ return {
     end,
   },
 
+  -- plugin to move windows around
+  {
+    "sindrets/winshift.nvim",
+    event = "VeryLazy",
+    config = true,
+    keys = {
+      {
+        "<leader>wsh",
+        "<cmd>WinShift left<cr>",
+        desc = "Shift window left",
+      },
+      {
+        "<leader>wsj",
+        "<cmd>WinShift down<cr>",
+        desc = "Shift window down",
+      },
+      {
+        "<leader>wsk",
+        "<cmd>WinShift up<cr>",
+        desc = "Shift window up",
+      },
+      {
+        "<leader>wsl",
+        "<cmd>WinShift right<cr>",
+        desc = "Shift window right",
+      },
+      {
+        "<leader>wsH",
+        "<cmd>WinShift far_left<cr>",
+        desc = "Shift window far left",
+      },
+      {
+        "<leader>wsJ",
+        "<cmd>WinShift far_down<cr>",
+        desc = "Shift window far down",
+      },
+      {
+        "<leader>wsK",
+        "<cmd>WinShift far_up<cr>",
+        desc = "Shift window far up",
+      },
+      {
+        "<leader>wsL",
+        "<cmd>WinShift far_right<cr>",
+        desc = "Shift window far right",
+      },
+    },
+  },
+  -- add description for whichkey
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>ws"] = { name = "+shift" },
+      },
+    },
+  },
+
   -- toggleable terminal widget
   {
     "akinsho/toggleterm.nvim",
