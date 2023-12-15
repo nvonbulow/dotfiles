113c5efccc420eb750ed42bf98912fab034ae4a3
nvonbulow <nick@nvonbulow.com>
2023-12-14 20:19:23 -0500

    Remove unused zsh config file

--- home/dot_zsh/exact_configs/pre/p10k-instant-prompt.zsh
+++
@@ -1,4 +0,0 @@
-# P10K_INSTANT_PROMPT_SRC="${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# if [[ -r $P10K_INSTANT_PROMPT_SRC ]]; then
-#     source $P10K_INSTANT_PROMPT_SRC
-# fi
