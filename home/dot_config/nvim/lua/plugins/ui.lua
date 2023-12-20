dd0b4c30eea47ff6dfa3d0c2680d96e64cbfe0a2
nvonbulow <nick@nvonbulow.com>
2023-12-19 22:31:00 -0500

    Add tmux plugins + nvim integration

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -118,4 +118,15 @@ return {
       },
     },
   },
+
+  -- integration with tmux
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to left window" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to lower window" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to upper window" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to right window" },
+    },
+  },
 }
