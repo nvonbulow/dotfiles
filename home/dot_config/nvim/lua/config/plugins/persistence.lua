b2eb2687bbe082e18d5bbcbf0d2d36725cd1e195
nvonbulow <nick@nvonbulow.com>
2023-12-25 21:11:10 -0500

    Fix neo-tree glitch with session restore

--- home/dot_config/nvim/lua/config/plugins/persistence.lua
+++ home/dot_config/nvim/lua/config/plugins/persistence.lua
@@ -3,10 +3,19 @@ return {
   {
     'folke/persistence.nvim',
     event = 'BufReadPre',
-    init = function ()
-      vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+    init = function()
+      vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp', 'folds' }
     end,
-    opts = { options = vim.opt.sessionoptions:get() },
+    ---@type PersistenceOptions
+    opts = {
+      options = vim.opt.sessionoptions:get(),
+      pre_save = function()
+        -- Close any neo-tree pane since this causes a bug upon session restore for some reason
+        if package.loaded['neo-tree'] then
+          require('neo-tree.command').execute({ action = 'close' })
+        end
+      end,
+    },
     keys = {
       {
         '<leader>qs',
