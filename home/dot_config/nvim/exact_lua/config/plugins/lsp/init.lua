3d84183dfb39e203fde4263fa81202e89b423f89
nvonbulow <nick@nvonbulow.com>
2023-12-28 16:08:37 -0500

    Custom nvim LSP icons

--- home/dot_config/nvim/exact_lua/config/plugins/lsp/init.lua
+++ home/dot_config/nvim/exact_lua/config/plugins/lsp/init.lua
@@ -119,6 +119,19 @@ return {
           })
         end,
       })
+
+      -- Customize
+      local signs = {
+        Error = ' ',
+        Warn = ' ',
+        Hint = ' ',
+        Info = ' ',
+      }
+
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
     end,
   },
 }
