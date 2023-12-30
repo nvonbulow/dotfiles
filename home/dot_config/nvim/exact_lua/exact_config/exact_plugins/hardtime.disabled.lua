c7dc452dedc1a5b51f461cc9a5d4ba4a8e40f7b8
nvonbulow <nick@nvonbulow.com>
2023-12-30 18:47:05 -0500

    Disable hardtime plugin due to incompatibility with which-key

--- home/dot_config/nvim/exact_lua/exact_config/exact_plugins/hardtime.disabled.lua
+++ home/dot_config/nvim/exact_lua/exact_config/exact_plugins/hardtime.disabled.lua
@@ -1,6 +1,8 @@
 return {
   {
     'm4xshen/hardtime.nvim',
+    -- I like the idea of this plugin, but it doesn't play well with which-key operators
+    cond = false,
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     opts = {},
   },
