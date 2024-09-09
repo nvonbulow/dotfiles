773bd3ec0e711e4c372028b6f75f56f81963bff9
nvonbulow <nick@nvonbulow.com>
2024-09-09 13:32:40 -0400

    Enable zen-mode for wezterm

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -7,12 +7,12 @@ local function get_config()
   end
   -- font settings
   config.font = wezterm.font("SpaceMono Nerd Font")
-  config.font_size = 15
+  config.font_size = 18
   -- disable ligatures ->
   config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
   config.underline_position = "-0.2cell"
   config.underline_thickness = "0.1cell"
-  config.line_height = 1.2
+  -- config.line_height = 1.2
 
   config.term = "wezterm"
 
@@ -23,5 +23,28 @@ local function get_config()
 
   return config
 end
+
+wezterm.on('user-var-changed', function(window, pane, name, value)
+  local overrides = window:get_config_overrides() or {}
+  if name == "ZEN_MODE" then
+    local incremental = value:find("+")
+    local number_value = tonumber(value)
+    if incremental ~= nil then
+      while (number_value > 0) do
+        window:perform_action(wezterm.action.IncreaseFontSize, pane)
+        number_value = number_value - 1
+      end
+      overrides.enable_tab_bar = false
+    elseif number_value < 0 then
+      window:perform_action(wezterm.action.ResetFontSize, pane)
+      overrides.font_size = nil
+      overrides.enable_tab_bar = true
+    else
+      overrides.font_size = number_value
+      overrides.enable_tab_bar = false
+    end
+  end
+  window:set_config_overrides(overrides)
+end)
 
 return get_config()
