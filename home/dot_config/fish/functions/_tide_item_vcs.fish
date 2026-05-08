6386e954581248709b99d95376a9cc6e8079f1bb
nvonbulow <nick@nvonbulow.com>
2026-05-08 11:15:52 -0400

    fish(tide): update vcs widget

--- home/dot_config/fish/functions/_tide_item_vcs.fish
+++ home/dot_config/fish/functions/_tide_item_vcs.fish
@@ -1,101 +1,79 @@
 function _tide_item_vcs
-    if not command -sq jj
-        _tide_item_git
-        return
-    end
+    # Set _tide_location_color which tide normally sets only when 'git' is in the items list.
+    # We replaced 'git' with 'vcs', so we must set it ourselves.
+    set_color $tide_git_color_branch | read -gx _tide_location_color
 
-    if not jj root --quiet >/dev/null 2>/dev/null
-        _tide_item_git
-        return
-    end
-
-    set -l change_id
-    jj log -r @ --no-graph -T 'change_id.shortest()' 2>/dev/null | read -l change_id
-    test -n "$change_id"; or return
-
-    set -l summary (jj diff --summary -r @ --ignore-working-copy 2>/dev/null)
-    set -l added (string match -r '^A ' $summary | count)
-    set -l modified (string match -r '^M ' $summary | count)
-    set -l removed (string match -r '^D ' $summary | count)
-    set -l renamed (string match -r '^R ' $summary | count)
-    set -l copied (string match -r '^C ' $summary | count)
-    set -l conflicts (jj resolve -r @ --list --ignore-working-copy 2>/dev/null | count)
-
-    set -g tide_vcs_bg_color $tide_git_bg_color
-    if test $conflicts -gt 0
-        set -g tide_vcs_bg_color $tide_git_bg_color_urgent
-    else if test (math "$added + $modified + $removed + $renamed + $copied") -gt 0
-        set -g tide_vcs_bg_color $tide_git_bg_color_unstable
-    end
-
-    set -l bookmark
-    set -l bookmark_color
-
-    # 1) Current changeset bookmark
-    set -l current_bookmarks (jj log -r @ --no-graph -T 'bookmarks' 2>/dev/null | string split ' ' | string match -rv '^$')
-    if test (count $current_bookmarks) -gt 0
-        set bookmark $current_bookmarks[1]
-        if contains -- main@origin $current_bookmarks
-            set bookmark main@origin
-        end
-        set bookmark_color $tide_git_color_branch
-    end
-
-    # 2) Closest parent bookmark (direct parent first, then nearest ancestor)
-    if test -z "$bookmark"
-        set -l parent_bookmarks (jj log -r '@-' --no-graph -T 'bookmarks' 2>/dev/null | string split ' ' | string match -rv '^$')
-        if test (count $parent_bookmarks) -eq 0
-            set parent_bookmarks (jj log -r 'heads(::@- & bookmarks())' --no-graph -n 1 -T 'bookmarks' 2>/dev/null | string split ' ' | string match -rv '^$')
-        end
-
-        if test (count $parent_bookmarks) -gt 0
-            set bookmark $parent_bookmarks[1]
-            if contains -- main@origin $parent_bookmarks
-                set bookmark main@origin
-            else
-                for b in $parent_bookmarks
-                    if string match -qr '@origin$' -- $b
-                        set bookmark $b
-                        break
-                    end
-                end
-            end
-            set bookmark_color $tide_git_color_staged
+    # Detect jj repo by walking parent dirs (deepest first) for .jj/
+    for i in (seq (count $_tide_parent_dirs) -1 1)
+        if test -d "$_tide_parent_dirs[$i]/.jj"
+            _tide_vcs_jj
+            return
         end
     end
 
-    # 3) Closest child bookmark
-    if test -z "$bookmark"
-        set -l child_bookmarks (jj log -r 'roots(@+:: & bookmarks())' --no-graph -n 1 -T 'bookmarks' 2>/dev/null | string split ' ' | string match -rv '^$')
-        if test (count $child_bookmarks) -gt 0
-            set bookmark $child_bookmarks[1]
-            if contains -- main@origin $child_bookmarks
-                set bookmark main@origin
-            end
-            set bookmark_color $tide_git_color_untracked
-        end
+    # Fall back to the original tide git item
+    _tide_item_git
+end
+
+function _tide_vcs_jj
+    # Metadata: change_id, bookmarks, conflicted file count, divergent, description
+    set -l jj_info (jj log -r '@' --no-graph --ignore-working-copy -T \
+        'change_id.shortest() ++ "\n" ++ bookmarks.map(|b| b.name()).join(",") ++ "\n" ++ self.conflicted_files().len() ++ "\n" ++ if(divergent, "divergent", "") ++ "\n" ++ description.first_line()' \
+        2>/dev/null)
+    or return
+
+    set -l change_id $jj_info[1]
+    set -l bookmarks $jj_info[2]
+
+    # File changes from jj diff --summary (snapshots working copy for live state)
+    set -l diff_lines (jj diff --summary 2>/dev/null)
+
+    # Parse counts using same regex pattern as tide's git item:
+    # (0|(?<name>...)) makes the named group empty when count is 0
+    string match -qr '(0|(?<conflicted>.*))\n(0|(?<added>.*))\n(0|(?<modified>.*))\n(0|(?<deleted>.*))' \
+        "$(echo $jj_info[3]
+        string match -r '^A ' $diff_lines | count
+        string match -r '^M ' $diff_lines | count
+        string match -r '^D ' $diff_lines | count)"
+
+    set -l divergent $jj_info[4]
+    set -l description $jj_info[5]
+
+    # Set bg color based on state
+    if test -n "$conflicted"
+        set -g tide_jj_bg_color $tide_jj_bg_color_urgent
+    else if test -n "$added$modified$deleted"
+        set -g tide_jj_bg_color $tide_jj_bg_color_unstable
     end
 
-    _tide_print_item vcs $_tide_location_color$tide_vcs_icon' ' (set_color white; echo -ns $tide_vcs_changeset_icon' '$change_id
-        if test -n "$bookmark"
-            set_color $bookmark_color; echo -ns ' '$tide_vcs_bookmark_icon' '$bookmark
+    # Build the display (matches git item pattern: symbol + count, empty when 0)
+    # Icons: f40c=alert_fill f457=diff_added f459=diff_modified f458=diff_removed f47a=bookmark_fill f417=git_commit
+    _tide_print_item jj $tide_jj_icon' ' (
+        if test -n "$bookmarks"
+            set_color $tide_jj_color_bookmark
+            echo -ns \uf47a' '$bookmarks' '
         end
-        if test $added -gt 0
-            set_color $tide_git_color_staged; echo -ns ' '$tide_vcs_change_added_icon' '$added
+        set_color $tide_jj_color_change_id
+        echo -ns \uf417' '$change_id
+        if test -n "$description"
+            set_color $tide_jj_color_description
+            echo -ns ' '(string shorten -m$tide_jj_truncation_length -- $description)
         end
-        if test $modified -gt 0
-            set_color $tide_git_color_dirty; echo -ns ' '$tide_vcs_change_modified_icon' '$modified
+        if test -n "$conflicted"
+            set_color $tide_jj_color_conflict; echo -ns ' '\uf40c' '$conflicted
         end
-        if test $removed -gt 0
-            set_color $tide_git_color_conflicted; echo -ns ' '$tide_vcs_change_removed_icon' '$removed
+        if test -n "$added"
+            set_color $tide_jj_color_added; echo -ns ' '\uf457' '$added
         end
-        if test $renamed -gt 0
-            set_color $tide_git_color_upstream; echo -ns ' '$tide_vcs_change_renamed_icon' '$renamed
+        if test -n "$modified"
+            set_color $tide_jj_color_modified; echo -ns ' '\uf459' '$modified
         end
-        if test $copied -gt 0
-            set_color $tide_git_color_untracked; echo -ns ' '$tide_vcs_change_copied_icon' '$copied
+        if test -n "$deleted"
+            set_color $tide_jj_color_deleted; echo -ns ' '\uf458' '$deleted
         end
-        if test $conflicts -gt 0
-            set_color $tide_git_color_conflicted; echo -ns ' '$tide_vcs_change_conflicted_icon' '$conflicts
-        end)
+        if test -n "$divergent"
+            set_color $tide_jj_color_divergent
+            echo -ns ' D'
+        end
+    )
 end
